5a90683f3f6404c12ef47eebb1b8a7020321aa41
Safdar Alee <112233435+AleeSafdar@users.noreply.github.com>
2023-03-18 11:48:26 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+Q(1):differentiating errors and bugs
+
+a bug refers to a flaw or defect in the code, while an error refers to a mistake or incorrect operation in the program's execution.(ChatGpt)
+
+Kinds of Bugs 
+Logical , SyntaxError , Runtime error
+
+1=Logical error occurs when the code doesnt give u the expected output due to wrong implementation of the code in the program. If suppose u have made the program of Multiplication and the Javascript is answering it in the positive form and it wont show any error in console so it is called a "difficult to debug error"
+
+
+2=Syntax error occurs if a developer violates the rules ex
+var 1name = "Safdar" // It doesnt appear to be the rule of variables
+
+3=Runtime error occurs when the program encounters an unexpected condition during its execution, such as trying to access an undefined var 
+
+
+
+
+  Q(2):Expressions and Statements
+Statements examples 
+any valid code in Javascript is called an statement
+let name= "Safdar ali"
+
+Expressions:
+if we are working on any nathematics evaluation like 
+2+2 or 10/2 these are called expressions . 
+  Logical expressions and fucntion calls are also its examples
+
